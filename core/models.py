588f4a9d31eda4305fa3bb2377d5adcd5ed84976
rootamin <osalimohammadamin@gmail.com>
2023-09-13 12:45:21 +0330

    added profile section to navbar, modified hero and events sections, created event model and registered them to admin, and configured media

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Events(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="events")
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = "Event"
+        verbose_name_plural = "Events"
+
+    def __str__(self):
+        return self.title
